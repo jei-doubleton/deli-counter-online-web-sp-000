42b1efdc2488f84383c81bfeef62828ed9b1b724
J <jennifer.singleton@gmail.com>
2019-01-13 19:01:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,11 @@
 # Write your code here.
 katz_deli = []
+
+def line(katz_deli, name)
+  if katz_deli.length == 0
+    "The line is currently empty."
+  else katz_deli.each do |number|
+    "Welcom, #{name}. You are number #{number} in line."
+  end
+  end
+end
