35576895fbc4aff4f6f0df256e91840fbc89361e
J <jennifer.singleton@gmail.com>
2019-01-13 19:08:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.empty
+  if katz_deli.empty?
     puts "The line is currently empty."
   else katz_deli.each_with_index do |name, index|
     puts "The line is currently: #{index}. #{name}"
