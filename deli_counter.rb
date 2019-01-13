63dfacaa67de8d0ebe90c12f2284e5cdcaebebb4
J <jennifer.singleton@gmail.com>
2019-01-13 19:10:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else katz_deli.each_with_index do |name, index|
-    puts "The line is currently: #{index+1}. #{name}"
-  end
+    deli_line >> "The line is currently: #{index+1}. #{name}"
+    end
+    deli_line
   end
 end
 
